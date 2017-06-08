5ebd750fd4162af9a8dc7ee7ea7c57e274a92475
Sandra <you@example.com>
2017-06-08 15:25:19 +0200

    Improve scripts and modify the whole code

--- app.js
+++ app.js
@@ -1,88 +1,48 @@
 // Express
 var express = require('express');
 var bodyParser = require('body-parser');
+
 var app = express();
-// Configure Express to use body-parser as middle-ware
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static('public'));
+// SQLite
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('database/db');
 
+// Functions
+
+
+
+// Gets & posts
 app.get('/', function (req, res) {
-    // res.send('Hello!');
-    // res.sendFile('index.html');
     res.sendFile('public/index.html', {'root': __dirname});
 });
 
 app.post('/', function(req, res) {
-    var nameTest = req.body.nameTest;
-    var passwordTest = req.body.passwordTest;
+    var name = req.body.name;
+    var phone = req.body.phone;
+    var address = req.body.address;
 
-    var nameTestChanged = nameTest + ' changed';
-    var passwordTestChanged = passwordTest + ' changed';
+    // todo
+    var addressDataDiv =
+        '<div class="address-no" id="counter">' +
+            '<img src="img/photo-name.png" alt="photo-alt">' +
 
-    var dataServer = {
-        nameTest: nameTestChanged,
-        passwordTest: passwordTestChanged
-    };
+            '<p>' + name + '</p>' +
+            '<p>' + phone + '</p>' +
+            '<p>' + address + '</p>' +
+            '<p>Groups</p>' +
 
-    res.send(dataServer);
+            '<input type="image" src="img/edit.jpg" alt="edit" class="button-edit">' +
+            '<input type="image" src="img/delete.jpg" alt="delete" class="button-delete">' +
+        '</div>';
 
-    // SQLite
-    var sqlite3 = require('sqlite3').verbose();
-    var db = new sqlite3.Database('database/db');
-
-    // SQLite - EX
-    // db.serialize(function() {
-    //     db.run("CREATE TABLE IF NOT EXISTS dataServer (name, password)");
-    //     db.run("INSERT INTO dataServer (name, password) VALUES (?, ?)", (dataServer.nameTest), (dataServer.passwordTest));
-    //
-    //     db.each("SELECT name, password FROM dataServer", function(err, row) {
-    //         console.log('column name: ' + row.name + ' column password: ' + row.password);
-    //     });
-    // });
-
-    // SQLite - EX
-    // db.serialize(function() {
-    //     db.run("CREATE TABLE IF NOT EXISTS dataServer (column1 TEXT)");
-    //
-    //     db.run("INSERT INTO dataServer (column1) VALUES (?)", "value1");
-    //     db.run("INSERT INTO dataServer (column1) VALUES (?)", "value2");
-    //
-    //     db.each("SELECT column1 FROM dataServer", function(err, row) {
-    //         console.log(row.column1);
-    //     });
-    // });
-
-    // SQLite - EX
-    // db.serialize(function() {
-    //     db.run("CREATE TABLE IF NOT EXISTS dataServer (column1, column2)");
-    //
-    //     db.run("INSERT INTO dataServer (column1, column2) VALUES ('value1', 'value2'), ('value11', 'value22')");
-    //
-    //     db.each("SELECT column1, column2 FROM dataServer", function(err, row) {
-    //         console.log('col1: ' + row.column1 + ', col2: ' + row.column2 + '\n');
-    //     });
-    // });
-
-    // SQLite - EX
-    // db.serialize(function() {
-    //     db.run("CREATE TABLE IF NOT EXISTS numbers (number TEXT)");
-    //
-    //     var stmt = db.prepare("INSERT INTO numbers VALUES (?)");
-    //     for (var i = 0; i < 10; i++) {
-    //         stmt.run("Number " + i);
-    //     }
-    //     stmt.finalize();
-    //
-    //     db.each("SELECT rowid AS id, number FROM numbers", function(err, row) {
-    //         console.log(row.id + ": " + row.number);
-    //     });
-    // });
-
-    db.close();
+    res.send(addressDataDiv);
 });
 
+// Run the server
 app.listen(3000, function() {
     console.log('The app is listening on port 3000!');
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,208 +1,105 @@
-// Get current local storage values (if they are set)
-if (localStorage.getItem('counter') === null) {
-    var counter = 0;
-} else {
-    var counter = localStorage.getItem('counter');
+// Show/hide HTML elements, disable/enable buttons
+function toggles(toggleAddressForm, firstAddressTextToggle, buttonsDisable, fillFieldsTextToggle) {
+      $('#addAddressForm').css('display', toggleAddressForm);
+      $('#addFirstAddressText').css('display', firstAddressTextToggle);
+      $('#button-add-address, .button-edit, .button-delete').prop('disabled', buttonsDisable);
+      $('#fill-in-fields-text').css('display', fillFieldsTextToggle);
 }
 
-if (localStorage.getItem('storedData') === null) {
-    var storedData = {};
-} else {
-    var storedData = JSON.parse(localStorage.getItem('storedData'));
+// Clear text input fields
+function clearFields() {
+    $('#add-address-name, #add-address-phone, #add-address-address').val('');
 }
 
-// Templates for adding addresses
-var addAddressForm =
-    '<div class="address-no" id="addAddressForm">' +
-        '<img src="" alt="">' +
-        '<p class="input-field">First and last name <input type="text" placeholder="First and last name" id="textinput-name"></p>' +
-        '<p class="input-field">Phone <input type="text" placeholder="Phone" id="textinput-phone"></p>' +
-        '<p class="input-field">Address <input type="text" placeholder="Address" id="textinput-address"></p>' +
-
-        '<p>Group(s)</p>' +
-        '<p class="checkbox"><input type="checkbox" name="group-checkbox" value="All groups" id="checkbox-all"> All addresses</p>' +
-        '<p class="checkbox"><input type="checkbox" name="group-checkbox" value="Group 1" id="checkbox-group1"> Group 1</p>' +
-        '<p class="checkbox"><input type="checkbox" name="group-checkbox" value="Group 2" id="checkbox-group2"> Group 2</p>' +
-        '<p class="checkbox"><input type="checkbox" name="group-checkbox" value="Group 3" id="checkbox-group3"> Group 3</p>' +
-
-        '<input type="image" src="img/save.png" alt="save" class="button-save">' +
-        '<input type="image" src="img/cancel.jpg" alt="cancel" class="button-cancel">' +
-    '</div>';
-
-var addressDiv =
-    '<div class="address-no" id="counter">' +
-        '<img src="img/photo-name.png" alt="photo-alt">' +
-
-        '<p>Name</p>' +
-        '<p>Phone</p>' +
-        '<p>Address</p>' +
-        '<p>Groups</p>' +
-
-        '<input type="image" src="img/edit.jpg" alt="edit" class="button-edit">' +
-        '<input type="image" src="img/delete.jpg" alt="delete" class="button-delete">' +
-    '</div>';
-
-// Display addresses from local storage
-function displayFromLocalStorage() {
-    var storedData = JSON.parse(localStorage.getItem('storedData'));
-    $.each(storedData, function(index, item) {
-        var addressDivCounter = addressDiv.replace('counter', index);
-        var addressDivUpdated = addressDivCounter.replace(/Name|Phone|Address/g, function(matched) {
-            return item[matched];
-        });
-        $('#address-wrapper').prepend(addressDivUpdated);
-    });
-}
-
-// Add the "Add your first address" text
-function addFirstAddress() {
-    var wrapperChildrenLength = $('#address-wrapper').children().length;
-    var addAddressFormLength = $('#addAddressForm').length;
-
-    if (wrapperChildrenLength == 0) {
-        $('#address-wrapper').html('<p id="addFirstAddress">Add your first address...</p>');
-    }
-
-    if (addAddressFormLength > 0) {
-        $('#addFirstAddress').remove();
-    }
-}
-
-// Display a form for entering addresses
+// Display a form for adding addresses
 function addAddress() {
-    $('#address-wrapper').prepend(addAddressForm);
-    $('#button-add-address').prop('disabled', true);
-    addFirstAddress();
-
-    $('.button-save').on('click', function(e) {
-        saveAddress(e);
-    });
-
-    $('.button-cancel').on('click', function() {
-        cancel();
-    });
+    toggles('block', 'none', true, 'none');
 }
 
-// Hide the form for entering addresses
-function cancel() {
-    $('#addAddressForm').remove();
-    $('#button-add-address').prop('disabled', false);
+// Cancel adding an address
+function addAddressCancel() {
+    function firstAddressTextToggle() {
+        if($('#address-wrapper').children().length) { // todo
+            return 'none';
+        } else {
+            return 'block';
+        }
+    }
+
+    toggles('none', firstAddressTextToggle() , false, 'none');
+    clearFields();
     displayOnce = 0;
-    addFirstAddress();
 }
 
-// Get data entered; display the data; save the data in local storage
+// Save an address
 var displayOnce = 0;
 function saveAddress(e) {
-    var name = document.getElementById('textinput-name').value;
-    var phone = document.getElementById('textinput-phone').value;
-    var address = document.getElementById('textinput-address').value;
+    displayOnce = 0;
+    var name = $('#add-address-name').val();
+    var phone = $('#add-address-phone').val();
+    var address = $('#add-address-address').val();
 
-    if(name == '' || phone == '' || address == '') {
+    if(name == '' || phone == '' || address == '') { // todo
         if(displayOnce <= 0) {
-            $('#addAddressForm').prepend('<p id="fill-in-fields">Please fill in all of the fields!</p>');
+            toggles('block', 'none', true, 'block');
             displayOnce++;
         }
         e.preventDefault();
     } else {
-        var items = {};
-        items.Name = name;
-        items.Phone = phone;
-        items.Address = address;
-        storedData[counter] = items;
+        toggles('none', 'none', false, 'none');
+        $('#add-address-name, #add-address-phone, #add-address-address').val('');
 
-        var addressDivCounter = addressDiv.replace('counter', counter);
-        var addressDivUpdated = addressDivCounter.replace(/Name|Phone|Address/g, function(matched) {
-            return items[matched];
-        });
-        $('#address-wrapper').prepend(addressDivUpdated);
-        counter++;
+        var addressData = {
+            name: name,
+            phone: phone,
+            address: address
+        }
 
-        localStorage.setItem('counter', counter);
-        localStorage.setItem('storedData', JSON.stringify(storedData));
-
-        $('#addAddressForm').remove();
-        $('#button-add-address').prop('disabled', false);
-        displayOnce = 0;
+        serverCommunication(addressData);
     }
-
-    $('.button-cancel').on('click', function() {
-        cancel();
-    });
-
-    $('.button-delete').on('click', function(e) {
-        deleteAddress(e);
-    });
-
-    $('.button-edit').on('click', function() {
-        editAddress();
-    });
 }
 
 // Delete an address
-function deleteAddress(e) {
-    var clickedButtonDiv = $(e.target).parent();
-    clickedButtonDiv.remove();
-    addFirstAddress();
+function deleteAddress() {
 
-    var storedData = JSON.parse(localStorage.getItem('storedData'));
-    var clickedButtonDivId = clickedButtonDiv.attr('id');
-    delete storedData[clickedButtonDivId];
-
-    localStorage.setItem('storedData', JSON.stringify(storedData));
 }
 
 // Edit an address
 function editAddress() {
-    // TBC
-    console.log('Edit an address');
+
 }
 
-// Send to the server and modify some data; save the data in the database
-function serverCommunication() {
-    nameTest = $("#name-test").val();
-    passwordTest = $("#password-test").val();
+// Save an edited address
+function editAddressSave() {
 
-    var dataFrontend = {
-        nameTest: nameTest,
-        passwordTest: passwordTest
-    };
-    var url = 'http://localhost:3000';
+}
 
+// Cancel editing an address
+function editAddressCancel() {
+
+}
+
+// Send address data to the server
+function serverCommunication(addressData) {
     $.ajax({
         type: 'post',
-        url: url,
-        data: dataFrontend,
-        dataType: 'json',
+        url: 'http://localhost:3000',
+        data: addressData,
+        dataType: 'text', // todo
         error: function() {
             console.log('error');
         },
-        success: function(dataServer) {
-            console.log('success');
-            console.log(dataServer);
-            $('#heading h1').text(JSON.stringify(dataServer.nameTest));
+        success: function(addressDataDiv) {
+            console.log(addressDataDiv);
+            $('#address-wrapper').prepend(addressDataDiv);
         }
     });
 }
 
 $(document).ready(function() {
-    var nameTest, passwordTest;
-    $("#submit-test").on('click', function() {
-        serverCommunication();
-    });
-
-    displayFromLocalStorage();
-    addFirstAddress();
-
-    $('#button-add-address').on('click', function() {
-        addAddress();
-    });
-
-    $('.button-delete').on('click', function(e) {
-        deleteAddress(e);
-    });
-
-    $('.button-edit').on('click', function() {
-        editAddress();
-    });
+    $('#button-add-address').on('click', addAddress);
+    $('.button-delete').on('click', deleteAddress);
+    $('.button-edit').on('click', editAddress);
+    $('#addAddressForm').children('.button-save').on('click', saveAddress);
+    $('#addAddressForm').children('.button-cancel').on('click', addAddressCancel);
 });
